31035a2bb26896d62df16fb270597a06d0d88b17
Taryn Cunha <taryn.cunha@gmail.com>
2022-07-21 15:45:31 -0400

    Added tests

---
+++ __tests__/App.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { App } from "../client/App";
+
+describe("App Component", function () {
+    it ("it should not have nav bar if the user hasn't signed in", function () {
+      let { getByText } = render(<App />);
+      expect (getByText())
+
+    })
